00fecbc608291779df3c3f18eb6d7df253b6e0f1
MohamedTarek <mohamedtarek20196046@gmail.com>
2021-11-22 00:53:18 +0200

    DataBase

--- src/DataBase.java
+++ src/DataBase.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 public class DataBase {
     HashMap<Integer, ArrayList<String>> Users = new HashMap<Integer, ArrayList<String>>();
     HashMap<Integer, ArrayList<String>> Drivers = new HashMap<Integer, ArrayList<String>>();
+    HashMap<Integer, ArrayList<String>> Rides = new HashMap<Integer, ArrayList<String>>();
 }
