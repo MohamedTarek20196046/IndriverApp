1fb3198951bb416f43b3cefc461ae493303a6047
MohamedTarek20196046 <94565639+MohamedTarek20196046@users.noreply.github.com>
2021-11-22 00:55:34 +0200

    Delete SystemUser.java

--- src/SystemUser.java
+++
@@ -1,7 +0,0 @@
-public abstract class SystemUser {
-     String Username;
-     String Password;
-     String Email;
-     String MobileNumber;
-     public void Login(String Username , String Password) {}
-}
