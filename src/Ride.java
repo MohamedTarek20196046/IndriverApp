72d00bb7729552ee58dc1b335af29c7b15c5e630
Abdallahhhassan <abdullahhassan436@gmail.com>
2021-11-22 01:04:05 +0200

    Ride class

--- src/Ride.java
+++ src/Ride.java
@@ -1,14 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ride {
     String SourceArea;
     String Destination;
     String Title;
     String Description;
     String TimeToLeave;
+    List<Integer> Offers = new ArrayList<Integer>();
     int Cost;
 
     public void RequestRide(String sourceArea,String destination)
     {
-
+        this.SourceArea=sourceArea;
+        this.Destination=destination;
     }
     public void AnnounceNewRide(String SourceArea,String Destination,String Title,String Description,String TimeToLeave,int Cost)
     {
