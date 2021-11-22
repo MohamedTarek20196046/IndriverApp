c0bbdc5d60125b5a832a664b298bc7153829857d
MohamedTarek <mohamedtarek20196046@gmail.com>
2021-11-22 18:49:58 +0200

    AdminUser

--- src/AdminUser.java
+++ src/AdminUser.java
@@ -1,7 +1,19 @@
 public class AdminUser {
-
-    public void VerifyDrivers()
+    int counter=1;
+    public void VerifyDrivers(UserManger m)
     {
+        for(int i=counter;i<m.DriverCount;i++)
+        {
+            if(m.data.Drivers.get(i).contains("approved"))
+            {
+                System.out.println("this Driver is already approved");
 
+            }
+            else
+            {
+                m.data.Drivers.get(i).add("approved");
+                counter++;
+            }
+        }
     }
 }
